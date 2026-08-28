0183fa0486a30bde32200aa82c7a81a4b5ac5fb9
Alaboussarsar <alaboussarsar@gmail.com>
2026-08-28 16:54:48 +0200

    Correction formatage Terraform (terraform fmt)

--- main.tf
+++ main.tf
@@ -24,18 +24,18 @@ provider "kubernetes" {
 module "ecs" {
   source = "./modules/ecs"
 
-  aws_region     = var.aws_region
-  account_id     = var.account_id
-  ecr_repo_name  = var.ecr_repo_name
-  image_tag      = var.image_tag
-  cluster_name   = var.ecs_cluster_name
-  desired_count  = var.ecs_desired_count
+  aws_region    = var.aws_region
+  account_id    = var.account_id
+  ecr_repo_name = var.ecr_repo_name
+  image_tag     = var.image_tag
+  cluster_name  = var.ecs_cluster_name
+  desired_count = var.ecs_desired_count
 }
 
 module "k8s" {
   source = "./modules/k8s"
 
-  app_image  = "nginxdemos/hello:plain-text"
-  replicas   = var.k8s_replicas
-  namespace  = var.k8s_namespace
+  app_image = "nginxdemos/hello:plain-text"
+  replicas  = var.k8s_replicas
+  namespace = var.k8s_namespace
 }
